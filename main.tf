c9e59d42bbd9e494ec4e3b9054a11dfdae35ac10
Joao Neto <netobat@MacBook-Air-Neto.local>
2022-04-23 08:33:18 -0300

    commit 2

--- main.tf
+++ main.tf
@@ -18,7 +18,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0892d3c7ee96c0bf7"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+#   user_data = <<-EOF
+#                  #!/bin/bash
+#                  cd /home/ubuntu
+#                  echo "<h1>Feito com terraform<h1>" > index.html
+#                  nohup busybox httpd -f -p 8080 &
+#                  EOF
   tags = {
-    Name = "PrimeiraInstancia"
+    Name = "Terraform Ansible Python"
   }
 }
